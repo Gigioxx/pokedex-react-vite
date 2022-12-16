481dcbf6d86c70cc3c9bb353a5d29a9167e7ab6c
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-16 17:24:45 -0300

    ✨ Displaying pokemon details

--- src/pages/PokemonDetails/index.tsx
+++ src/pages/PokemonDetails/index.tsx
@@ -1,8 +1,13 @@
+import { useParams } from 'react-router-dom';
+import { PokemonDetail } from '../../components/PokemonDetail';
+import { usePokemon } from '../../hooks/usePokemon';
+
 export const PokemonDetails = () => {
-  return (
-    <>
-      <div>Details</div>
-      <div>page</div>
-    </>
-  );
+  const { pokemonId } = useParams();
+  const { pokemon } = usePokemon('', pokemonId);
+
+  if (!pokemon)
+    return <span>{pokemonId} is not a valid Pokemon ID or Pokemon Name</span>;
+
+  return <PokemonDetail pokemon={pokemon} />;
 };
