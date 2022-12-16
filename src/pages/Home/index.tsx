f45fb56177ed9be26a2f1889f09e5ce3b00f8f06
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-16 16:43:35 -0300

    ✨ Using pagination in homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,38 @@
 import { useContext } from 'react';
 import { SmallPokeballIcon } from '../../assets/pokeball';
+import { Pagination } from '../../components/Pagination';
 import { PokemonList } from '../../components/PokemonList';
 import { PokemonContext } from '../../context/PokemonContext';
+import { usePagination } from '../../hooks/usePagination';
 
 import styles from './styles.module.scss';
 
 export const Home = () => {
   const { filteredPokemon } = useContext(PokemonContext);
+  const { page, nextPage, previousPage, backToHome } = usePagination();
+
+  let pokemonPerPage = 12;
 
   return (
     <div className={styles.home}>
       <header>
-        <div>
+        <div onClick={backToHome}>
           <SmallPokeballIcon />
           <span>Pokédex</span>
         </div>
       </header>
-      <PokemonList pokemonUrls={filteredPokemon} />
+      <PokemonList
+        page={page}
+        pokemonPerPage={pokemonPerPage}
+        pokemonUrls={filteredPokemon}
+      />
+      <Pagination
+        page={page}
+        pokemonPerPage={pokemonPerPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+        maxItems={filteredPokemon?.length!}
+      />
     </div>
   );
 };
