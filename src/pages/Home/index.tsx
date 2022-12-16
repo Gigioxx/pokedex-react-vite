7f59db20c8badcd0c920e6fa054cb885aaf8364e
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-16 16:11:54 -0300

    ✨ Added styles and displaying pokemons in homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
+import { SmallPokeballIcon } from '../../assets/pokeball';
+import { PokemonList } from '../../components/PokemonList';
+import { PokemonContext } from '../../context/PokemonContext';
+
+import styles from './styles.module.scss';
+
 export const Home = () => {
+  const { filteredPokemon } = useContext(PokemonContext);
+
   return (
-    <>
-      <div>home</div>
-      <div>page</div>
-    </>
+    <div className={styles.home}>
+      <header>
+        <div>
+          <SmallPokeballIcon />
+          <span>Pokédex</span>
+        </div>
+      </header>
+      <PokemonList pokemonUrls={filteredPokemon} />
+    </div>
   );
 };
